4cba11b3abc8e4533fdd8212147c240812e593ae
tareqkian <64751211+tareqkian@users.noreply.github.com>
2023-10-01 14:08:35 +0300

    fixing bugs

--- @modules/auth/composables/useAuth.ts
+++ @modules/auth/composables/useAuth.ts
@@ -4,8 +4,15 @@ type Credentials = {
   password: string
   password_confirmation?: string
 }
+type User = {
+  user: any
+  token: string
+  error: any,
+  loading: boolean
+}
 export class Auth {
   constructor(
+    private user: User
   ) {}
 
   login = async (credentials: Credentials) => await useBaseApi('/auth/login', { method: "POST", body: credentials })
--- @modules/auth/stores/auth-store.ts
+++ @modules/auth/stores/auth-store.ts
@@ -18,28 +18,27 @@ export const useAuthStore = defineStore('auth', () => {
     loading: false
   })
 
-  const setUser = (userData: any, error: any) => {
-    user.user = userData?.data.user
-    user.token = userData?.data.token
+  const setUser = (data: any, error: any) => {
+    user.user = data?.user
+    user.token = data?.token
     user.error = error
     user.loading = false
-    if ( userData?.data.token ) navigateTo('/')
+    if ( data?.token ) navigateTo('/')
   }
 
   const login = async (credentials: Credentials) => {
     user.loading = true
-    const { data, error } = await new Auth().login(credentials)
-    console.log(data, error)
+    const { data, error } = await new Auth(user).login(credentials)
     setUser(data.value, error.value?.data.errors)
   }
   const register = async (credentials: Credentials) => {
     user.loading = true
-    const { data, error } = await new Auth().register(credentials)
+    const { data, error } = await new Auth(user).register(credentials)
     setUser(data.value, error.value?.data.errors)
   }
   const logout = async () => {
     user.loading = true
-    const { error } = await new Auth().logout()
+    const { error } = await new Auth(user).logout()
     user.user = ''
     user.token = ''
     user.error = error.value?.data.errors
